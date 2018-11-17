9d3d8627abe4c41ce4c4964e03c650fd665d51ae
chenlipin2015@gmail.com <chenlipin2015@gmail.com>
2018-11-17 14:27:18 +0800

    [Add]模块热替换的迷思。为什么guide里面说两份代码是不一样的。我没看出来区别呀

---
+++ 6.HMR/webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const ManifestPlugin = require('webpack-manifest-plugin');
+const webpack = require('webpack');
+
+module.exports = {
+        entry: {
+            app: './src/index.js'
+        },
+        plugins: [
+            new ManifestPlugin(),
+            new CleanWebpackPlugin(['dist']),
+            new HtmlWebpackPlugin({
+              title: 'Output Management'
+         }),
+            new webpack.NamedModulesPlugin(),
+            new webpack.HotModuleReplacementPlugin()
+        ],
+       /* devServer: {
+            contentBase: './dist'
+        },*/
+        devtool: 'inline-source-map',
+        output: {
+            filename: '[name].bundle.[hash].js',
+            path: path.resolve(__dirname, 'dist'),
+            publicPath: '/'
+        }
+};
